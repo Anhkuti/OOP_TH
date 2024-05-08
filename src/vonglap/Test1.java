9b8d5157cef72f6369f0fca24b1c45d841e670e8
Anhkuti <anhmoccanh@gmail.com>
2024-05-08 19:30:29 +0700

    update

---
+++ src/vonglap/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+    public static void main(String[] args) {
+        double[] numbers = {3.5, 8.9, 1.6, -4.8};
+
+        // access array elements using the for loop
+        for (int i = 0; i <= numbers.length - 1; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
